deec1b140dff42da6502eec54e32d250740cee12
mariosaldanha10 <mariosaldanha.ti@gmail.com>
2021-11-02 20:39:53 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ReadFile003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ///  FileDemo.ReadDemo("jamaica.txt");
+
+
+            string dc_heroesFile = "dc.txt";
+            string[] dc_heroes = { "Batman:Bruce Wayne", "Superman:Clarke Kent", "Aquaman:Arthur Curry" };
+
+            FileDemo.WriteDemo(dc_heroesFile, dc_heroes);
+            FileDemo.ReadDemo(dc_heroesFile);
+
+
+
+
+
+            string marvel_heroesFile = "heroes.txt";
+            string[] marvel_heroes = { "SpiderMan:Peter Parker", "The Hulk:David Banner", "Captain America:Steve Rogers" };
+            FileDemo.WriteDemo(marvel_heroesFile, marvel_heroes);
+
+            FileDemo.ReadDemo(marvel_heroesFile);
+
+
+
+
+            FileDemo.OutputSeparator();
+            char[] characters = FileDemo.SplitAsString("supercalifragilisticexpialidocious");
+
+            Console.WriteLine($"The character at index position 10, live position 11 is {characters[10]}");
+
+
+
+
+
+        }
+    }
+}
